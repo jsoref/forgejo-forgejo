fb52adb57b08593216a0dcd65132472613170322
Earl Warren <contact@earl-warren.org>
2023-02-07 11:23:49 +0100

    [API] Forgejo API /api/forgejo/v1
    (cherry picked from commit 20b56692693e054bb3c04b4ef12b29b0715b4530)
    (cherry picked from commit 1574643a6a9634e5b92c033a4bfb69062a86bd05)
    
    Update semantic version according to specification
    
    (cherry picked from commit 22510f41306f9c133a7e99b61f9c38dabfd4b810)
    
    Mise à jour de 'Makefile'
    
    (cherry picked from commit c3d85d8409f1bb18a45659a167cf1ffee057f3b6)
    (cherry picked from commit 5ea23098513c068444226af41faf9be9c4c998e6)
    (cherry picked from commit ec5217b9d1b94bb04e34ce8c27eecbdc6f3a247a)
    (cherry picked from commit 14f08e364b2ad7e3176b45d3ec8da5f5605b4315)
    (cherry picked from commit b4465c67b8737aed9fd79a43046aa3795a298e68)
    
    [API] [SEMVER] replace number with version
    
    (cherry picked from commit fba48e64977a75e77c9c6cf8e3f8a679a5e7b733)
    (cherry picked from commit 532ec5d8782703b62d7d02312764e50637cd016e)
    
    [API] [SEMVER] [v1.20] less is replaced by css
    
    (cherry picked from commit 01ca3a4f4291cf4c62df42cbd12a00638f3cdc70)
    (cherry picked from commit 1d928c3ab2e1fda6082e1400d89ea96d6605ebaa)
    (cherry picked from commit a39dc804cd1da86444ed709455857f069475e7db)
    
    Conflicts:
    	webpack.config.js
    (cherry picked from commit adc68578b3eb41215338125edfca91804706f8a1)
    (cherry picked from commit 9b8d98475fd7efcc15bf0d91b0af6ef3aff64508)
    (cherry picked from commit 25161039745a63282257eded9616390241af8013)
    (cherry picked from commit 18e6287963a0e1863cd08fd514a4e732a1904a68)
    (cherry picked from commit e9694e67ab93ad74f7eae3152d88d95e8493e739)
    (cherry picked from commit a9763edaf002c8009a2cca4115cb8b926f8cdc85)
    (cherry picked from commit e2b550f4fba144f9637a212b56bfe30cac1adb3b)
    (cherry picked from commit 2edac36701e8fa1ff67f87dccfa34cfc00ab66de)
    
    [API] Forgejo API /api/forgejo/v1 (squash)
    
    Update semver as v1.20 is entering release candidate mode
    
    (cherry picked from commit 4995098ec3c6eee19a7ad7bc956b71c6e937c04c)
    (cherry picked from commit 3dd6f2862e10250e5caefbf46a6c8092ab68eabc)
    (cherry picked from commit e8192737ef5773b520f85c6ea405219ad6fce1a5)
    (cherry picked from commit 7b1245cc706c95e70c01601c54e0dfabb162ba31)
    (cherry picked from commit 4d7cd59703dffbd2ab5bb417afe8ace3d0af7141)
    (cherry picked from commit 7a7a293dbd8f5e2acd313d3d96c54a337f717f1f)

--- routers/init.go
+++ routers/init.go
@@ -30,6 +30,7 @@ import (
 	"code.gitea.io/gitea/modules/translation"
 	"code.gitea.io/gitea/modules/web"
 	actions_router "code.gitea.io/gitea/routers/api/actions"
+	forgejo "code.gitea.io/gitea/routers/api/forgejo/v1"
 	packages_router "code.gitea.io/gitea/routers/api/packages"
 	apiv1 "code.gitea.io/gitea/routers/api/v1"
 	"code.gitea.io/gitea/routers/common"
@@ -175,6 +176,7 @@ func NormalRoutes(ctx context.Context) *web.Route {
 
 	r.Mount("/", web_routers.Routes(ctx))
 	r.Mount("/api/v1", apiv1.Routes(ctx))
+	r.Mount("/api/forgejo/v1", forgejo.Routes(ctx))
 	r.Mount("/api/internal", private.Routes())
 
 	if setting.Packages.Enabled {
