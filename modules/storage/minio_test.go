79bbbe2dd2692b5fc1db40fae7cc598f1cb76a1b
Earl Warren <contact@earl-warren.org>
2023-05-16 08:33:22 +0200

    [CI] disable minio test, no minio server yet in CI
    (cherry picked from commit 0dd74d09d363433b1b8730bd26a6d7865e1e5ca0)
    (cherry picked from commit 28d2d9b212e168fcc56a280c5750716e70a75f8d)
    (cherry picked from commit 3c3db1ff0f5f7d2490895a64498446f41ac6c30c)

--- modules/storage/minio_test.go
+++ modules/storage/minio_test.go
@@ -4,15 +4,11 @@
 package storage
 
 import (
-	"os"
 	"testing"
 )
 
 func TestMinioStorageIterator(t *testing.T) {
-	if os.Getenv("CI") == "" {
-		t.Skip("minioStorage not present outside of CI")
-		return
-	}
+	t.Skip("minio not found in Forgejo test yet")
 	testStorageIterator(t, string(MinioStorageType), MinioStorageConfig{
 		Endpoint:        "127.0.0.1:9000",
 		AccessKeyID:     "123456",
