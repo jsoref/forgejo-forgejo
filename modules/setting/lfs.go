f4fcdaba8cfde61a1b80d5b72efb35b3531cd4e7
Earl Warren <contact@earl-warren.org>
2023-07-30 07:42:38 +0200

    Revert "[GITEA] do not use deprecatedSettingFatal for cosmetic reasons"
    This reverts commit 2de8602855140a5addb8a1d12405e90da4fe0f69.

--- modules/setting/lfs.go
+++ modules/setting/lfs.go
@@ -34,14 +34,7 @@ func loadLFSFrom(rootCfg ConfigProvider) error {
 	// Specifically default PATH to LFS_CONTENT_PATH
 	// DEPRECATED should not be removed because users maybe upgrade from lower version to the latest version
 	// if these are removed, the warning will not be shown
-	deprecatedSetting(rootCfg, "server", "LFS_CONTENT_PATH", "lfs", "PATH", "v1.19.0")
-
-	if val := sec.Key("LFS_CONTENT_PATH").String(); val != "" {
-		if lfsSec == nil {
-			lfsSec = rootCfg.Section("lfs")
-		}
-		lfsSec.Key("PATH").MustString(val)
-	}
+	deprecatedSettingFatal(rootCfg, "server", "LFS_CONTENT_PATH", "lfs", "PATH", "v1.19.0")
 
 	var err error
 	LFS.Storage, err = getStorage(rootCfg, "lfs", "", lfsSec)
