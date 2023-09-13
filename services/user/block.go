8ad87d215f2b6adb978de77e53ba2bf7ea571430
Gusted <postmaster@gusted.xyz>
2023-09-13 20:11:32 +0200

    [MODERATION] Add repo transfers to blocked functionality (squash)
    - When someone gets blocked, remove all pending repository transfers
    from the blocked user to the doer.
    - Do not allow to start transferring repositories to the doer as blocked user.
    - Added unit testing.
    - Added integration testing.
    
    (cherry picked from commit 8a3caac33013482ddbee2fa51510c6918ba54466)
    (cherry picked from commit a92b4cfeb63b90eb2d90d0feb51cec62e0502d84)
    (cherry picked from commit acaaaf07d999974dbe5f9c5e792621c597bfb542)
    (cherry picked from commit 735818863c1793aa6f6983afedc4bd3b36026ca5)
    (cherry picked from commit f50fa43b32160d0d88eca1dbdca09b5f575fb62b)
    (cherry picked from commit e16683643388fb3c60ea478f1419a6af4f4aa283)
    (cherry picked from commit 82a0e4a3814a66ce44be6a031bdf08484586c61b)
    (cherry picked from commit ff233c19c4a5edcc2b99a6f41a2d19dbe8c08b3b)

--- services/user/block.go
+++ services/user/block.go
@@ -5,9 +5,12 @@ package user
 import (
 	"context"
 
+	model "code.gitea.io/gitea/models"
 	"code.gitea.io/gitea/models/db"
 	repo_model "code.gitea.io/gitea/models/repo"
 	user_model "code.gitea.io/gitea/models/user"
+
+	"xorm.io/builder"
 )
 
 // BlockUser adds a blocked user entry for userID to block blockID.
@@ -62,6 +65,28 @@ func BlockUser(ctx context.Context, userID, blockID int64) error {
 	}
 
 	_, err = db.GetEngine(ctx).In("id", collabsID).Delete(&repo_model.Collaboration{})
+	if err != nil {
+		return err
+	}
+
+	// Remove pending repository transfers, and set the status on those repository
+	// back to ready.
+	pendingTransfersIDs, err := model.GetPendingTransferIDs(ctx, userID, blockID)
+	if err != nil {
+		return err
+	}
+
+	// Use a subquery instead of a JOIN, because not every database supports JOIN
+	// on a UPDATE query.
+	_, err = db.GetEngine(ctx).Table("repository").
+		In("id", builder.Select("repo_id").From("repo_transfer").Where(builder.In("id", pendingTransfersIDs))).
+		Cols("status").
+		Update(&repo_model.Repository{Status: repo_model.RepositoryReady})
+	if err != nil {
+		return err
+	}
+
+	_, err = db.GetEngine(ctx).In("id", pendingTransfersIDs).Delete(&model.RepoTransfer{})
 	if err != nil {
 		return err
 	}
